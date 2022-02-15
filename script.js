4fc1c97cee48dc0edaf4901b19887f4abae4bd8b
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-15 20:26:28 -0300

    Task 7 of project done

--- script.js
+++ script.js
@@ -7,14 +7,26 @@ function createDivs(className) {
 
 function createPixelBoard(n) {
     for (let i = 0; i < n; i += 1) {
-        document.querySelector('#pixel-board').appendChild(createDivs('line'))
+        document.querySelector('#pixel-board').appendChild(createDivs('line'));
     }
-    let lines = document.querySelectorAll('.line')
+    let lines = document.querySelectorAll('.line');
     for (let line of lines) {
         for (let i1 = 0; i1 < n; i1 +=1) {
-            line.appendChild(createDivs('pixel'))
+            line.appendChild(createDivs('pixel'));
         }
     }
 }
 createPixelBoard(5);
 
+//7 Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+let colorOption = document.querySelectorAll('.color');
+for (let colSel of colorOption) {
+    colSel.addEventListener('click', classSeleted);
+}
+
+function classSeleted (event) {
+    for (let colSel of colorOption) {
+        colSel.className = 'color';
+    }
+    event.target.className = 'color selected'
+}
